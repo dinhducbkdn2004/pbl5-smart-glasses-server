f9e46ec2baa143b317aef976a74fc7c925d56f75
DinhDuc <dinhducbkdn2004@gmail.com>
2025-03-01 11:49:39 +0700

    init-docker-image

--- app/main.py
+++ app/main.py
@@ -4,5 +4,10 @@ from app.api.route import router
 # Khởi tạo FastAPI app
 app = FastAPI()
 
+@app.get("/")
+async def index():
+    return {"message": "Server listening on port 8000"}
+
+
 # Bao gồm các route từ file route.py
 app.include_router(router)
